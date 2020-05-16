88c347d11072db537391c7dfdc3b1a8aeea7f641
amritadhir116 <amritadhir93@gmail.com>
2020-05-15 20:32:08 -0400

    final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { routing } from './routing';
+
+function App() {
+  return (
+    <div className="App">
+      {routing}
+    </div>
+  );
+}
+
+export default App;
